790e2cc9db56c5b1a7b43d69bb9fc1be722058c1
cahoojac000 <jacobbcahoon@gmail.com>
2023-03-27 21:26:10 -0600

    added multithreading capabilities to make download go vroom vrrom

--- image_prep.jl
+++ image_prep.jl
@@ -1,26 +1,24 @@
 using CSV
-using FileIO
-using Images
 using DataFrames
-using Flux
-using QuartzImageIO
-using PyCall
 using HTTP
-using ImageView
+using Images
 
-function prepare(url)
-    local response = HTTP.get(url)
-    local image_data = response.body
-    local image = load(IOBuffer(image_data))
-    local resized_image = imresize(image, (224, 224))
+function prepare(response, iteration)
+    image_data = response.body
+    image = load(IOBuffer(image_data))
+    resized_image = imresize(image, (224, 224))
 
-    println("Finished with image")
+    println("Finished with image at iteration: $iteration")
 
     return resized_image
 end
 
 df = CSV.read("labeled_images.txt", DataFrame, header=true)
 
-df.PixelVals = [prepare(url) for url in df.urls]
+# Create an array of tasks
+tasks = [@async prepare(fetch(@async HTTP.get(url)), i) for (i, url) in enumerate(df.urls)]
+
+# Wait for all tasks to complete and collect results
+df.PixelVals = [fetch(t) for t in tasks]
 
 CSV.write("prepared_images.csv", df[:, [:PixelVals, :Labels]])
